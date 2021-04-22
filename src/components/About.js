f4d63d7506064a48e6fc08556619b30bd2c523af
Miglemeta <miglezalubiene@gmail.com>
2021-04-22 23:03:11 +0300

    feat/added text in about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,19 @@
-import React from "react"
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 function About() {
   return (
-    <div>
-      <h1>About Page </h1>
+    <div className="about">
+      <h1>About Me </h1>
+      <h2>
+        My parents both were artists, probably that's where I got my love for
+        crafting since I was a little child. I was noticed in school for my
+        abilities to draw. Somehow destiny was played for me, that I will have
+        to do more work in law and social work and creating become to be my
+        hobby after work. Crafting inspires me to love life and others more and
+        I love to share this amazing mood with others. I hope it will inspire
+        you to make something yourself.
+      </h2>
     </div>
   );
 }
